0fadc964d50fc388798e7786fed03a9aebd78fb5
Angel Zambrano <angelhzambrano@gmail.com>
2026-06-14 13:06:15 -0300

    Fix reportes URL routing - add base path redirect to asistencias

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'reportes'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='asistencias/', permanent=False), name='index'),
     path('asistencias/', views.reporte_asistencias, name='asistencias'),
     path('cobros/', views.reporte_cobros, name='cobros'),
     path('membresias-vencidas/', views.membresias_vencidas, name='membresias_vencidas'),
